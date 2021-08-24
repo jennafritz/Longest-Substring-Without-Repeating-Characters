a1f844f7f15bf677e31447c637ad2d178bc6cbf9
jennafritz <jennafritz24@gmail.com>
2021-08-24 12:10:12 -0400

    Completed JS

--- javascript/longSubString.js
+++ javascript/longSubString.js
@@ -1,5 +1,28 @@
 function lengthOfLongestSubstring(s) {
   // type your code here
+  let longestSubstring = ""
+
+  let substring = ""
+
+  let substringSet = new Set()
+  
+  for(j = 0; j < s.length; j++){
+    substring = s[j]
+    substringSet.add(s[j])
+    for(i = j + 1; i < s.length; i++){
+      if(substringSet.has(s[i])){
+        if(substring.length > longestSubstring.length){
+          longestSubstring = substring
+        }
+        substringSet.clear()
+        break
+      } else {
+        substringSet.add(s[i])
+        substring += s[i]
+      }
+    }
+  }
+  return longestSubstring.length
 }
 
 if (require.main === module) {
@@ -7,6 +30,16 @@ if (require.main === module) {
 // add your own tests in here
 console.log("Expecting: 3");
 console.log("=>", lengthOfLongestSubstring("abcabcbb"));
+
+console.log("")
+
+console.log("Expecting: 3");
+console.log("=>", lengthOfLongestSubstring("pwwkew"));
+
+console.log("")
+
+console.log("Expecting: 0");
+console.log("=>", lengthOfLongestSubstring(""));
 
 }
 
